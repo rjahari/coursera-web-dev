7e6ef5961d16c877cca00d188797ca47cc47b9d2
R. Jahari <112365686+rjahari@users.noreply.github.com>
2023-09-13 18:39:33 +0530

    special category click loading fixed

--- assignment5/js/script.js
+++ assignment5/js/script.js
@@ -123,8 +123,6 @@ function buildAndShowHomeHTML (categories) {
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
-      console.log(homeHtmlToInsertIntoMainPage)
-
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
